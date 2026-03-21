dcbd5cbfaff57786adfbb3efd392b4103064c2a3
GalaletsangM <preciousmafoko@gmail.com>
2026-03-21 02:05:49 -0400

    commit

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -4,10 +4,10 @@ import { PillProps } from "@/interfaces";
 //  title: string;
 //}
 
-const Pill: React.FC<PillProps> = ({ title }) => {
+const Pill: React.FC<PillProps> = ({title}) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   );
 };
